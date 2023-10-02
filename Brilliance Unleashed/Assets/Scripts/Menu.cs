7c8e028baaa5cdd2643d612c95436a1a063d8cbe
WadedOudhuis <W.J.Oudhuis@student.tudelft.nl>
2023-10-02 16:28:27 +0200

    Add very basic menu

---
+++ Brilliance Unleashed/Assets/Scripts/Menu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void OnPlayButton ()
+    {
+        SceneManager.LoadScene(1);
+
+
+    }
+
+    public void onQuitButton()
+    {
+        Application.Quit();
+    }
+
+
+}
