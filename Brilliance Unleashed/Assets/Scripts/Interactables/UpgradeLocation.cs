95d4740175bad885ff8aef24eccfafc67ff858c1
Nina Immig <N.M.Immig@student.tudelft.nl>
2023-10-02 23:55:07 +0200

    Last Sounds

--- Brilliance Unleashed/Assets/Scripts/Interactables/UpgradeLocation.cs
+++ Brilliance Unleashed/Assets/Scripts/Interactables/UpgradeLocation.cs
@@ -7,6 +7,8 @@ public class UpgradeLocation : Interactable
     [SerializeField]
     UIShop uiShop;
 
+    [SerializeField] AudioSource audioSource;
+
     bool active = false;
 
 
@@ -17,6 +19,8 @@ public class UpgradeLocation : Interactable
         else uiShop.Hide();
 
         active = !active;
+
+        audioSource.PlayOneShot(audioSource.clip, 1);
 
     }
 
